5dcd8c6c3ec7f8e7f5cfc560fad0d8736575bc52
jessijoke <jessijoke@yahoo.com>
2021-12-27 01:45:03 -0500

    Initial commit

---
+++ src/components/StatsPreviewCard.js
@@ -0,0 +1,18 @@
+import '../styles/statsPreviewCardStyles.css'
+
+export const StatsPreviewCard = (props) => {
+    return(
+        <div className="statsCardContainer">
+            <div className="mainStatCard">
+                <div className="stats">
+                    Test1
+                </div>
+                <div className="imageContainer">
+                    <div className="backgroundImage">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
