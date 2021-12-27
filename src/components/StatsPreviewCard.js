81d186d812ab1a7f5bd0a59189568556ff0a0f35
jessijoke <jessijoke@yahoo.com>
2021-12-27 02:07:20 -0500

    Copy templated in

--- src/components/StatsPreviewCard.js
+++ src/components/StatsPreviewCard.js
@@ -5,7 +5,16 @@ export const StatsPreviewCard = (props) => {
         <div className="statsCardContainer">
             <div className="mainStatCard">
                 <div className="stats">
-                    Test1
+                    <div className="statsContainer">
+                        <div className="statsHeader">
+                        Get insights that help your business grow.
+                        </div>
+                        <div className="statsDescription">
+                        Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.
+                        </div>
+                        <div className="statsInfo">
+                        </div>
+                    </div>
                 </div>
                 <div className="imageContainer">
                     <div className="backgroundImage">
