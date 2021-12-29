8bed61cee1c662ddbf9f3f114683a6bb3a831896
jessijoke <jessijoke@yahoo.com>
2021-12-29 00:17:02 -0500

    Tweaking styling to finish

--- src/components/StatsPreviewCard.js
+++ src/components/StatsPreviewCard.js
@@ -6,13 +6,37 @@ export const StatsPreviewCard = (props) => {
             <div className="mainStatCard">
                 <div className="stats">
                     <div className="statsContainer">
-                        <div className="statsHeader">
+                        <div className="statsHeader interFont headTitle">
                         Get insights that help your business grow.
                         </div>
-                        <div className="statsDescription">
+                        <div className="statsDescription detailInfo">
                         Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.
                         </div>
-                        <div className="statsInfo">
+                        <div className="statsInfo headerFont">
+                            <div className="statsInfoContainer">
+                                <div className="statHeader">
+                                10k+
+                                </div>
+                                <div className="statDetails detailInfo">
+                                companies
+                                </div>
+                            </div>
+                            <div className="statsInfoContainer">
+                                <div className="statHeader">
+                                314
+                                </div>
+                                <div className="statDetails detailInfo">
+                                templates
+                                </div>
+                            </div>
+                            <div className="statsInfoContainer">
+                                <div className="statHeader">
+                                12M+
+                                </div>
+                                <div className="statDetails detailInfo">
+                                queries
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
